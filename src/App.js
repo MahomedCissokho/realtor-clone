0df8765020712abc1b67b342c10b6c88745ffab4
Mahomed Cissokho <mahomedcissokho@esp.sn>
2023-06-05 18:09:12 -0400

    installation of react and tailwindcss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <>
+      <p className='text-3xl underline text-yellow-600 cursor-pointer'>Hello, my name is Mahomed Cissokho </p>
+    </>
+  )
+}
+
+export default App;
